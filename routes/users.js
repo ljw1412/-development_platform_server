844040ece59a6cd016ca05c89572f676925b6784
ljw1412 <506644791@qq.com>
2019-06-14 19:19:24 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,21 @@ const User = dbHelper.getModel('User')
 router.prefix('/users')
 
 router.get('/add', async (ctx, next) => {
-  const { username, password } = ctx.query
-  if (username && password) {
-    await User.findOne({ username })
-      .then(user =>
-        user
-          ? Promise.reject({ error: `username ${username} is exits` })
-          : User.create({ username, password })
-      )
-      .then(() => {
-        ctx.body = { message: `user ${username} create success.` }
-      })
-      .catch(err => {
-        ctx.body = err
-      })
+  const { username, password, email } = ctx.query
+  if (username && password && email) {
+    const users = await User.find({ $or: [{ username }, { email: username }] })
+    // await User.findOne({ username })
+    //   .then(user =>
+    //     user
+    //       ? Promise.reject({ error: `username ${username} is exits` })
+    //       : User.create({ username, password })
+    //   )
+    //   .then(() => {
+    //     ctx.body = { message: `user ${username} create success.` }
+    //   })
+    //   .catch(err => {
+    //     ctx.body = err
+    //   })
   } else {
     ctx.body = { error: 'plase input username or password' }
     // ctx.throw(500, `plase input username or password`)
@@ -28,16 +29,13 @@ router.get('/add', async (ctx, next) => {
 
 router.post('/login', async (ctx, next) => {
   const { username, password } = ctx.request.body
-  await User.findOne({ username, password })
-    .then(
-      user => user || Promise.reject({ error: 'username or password error' })
-    )
-    .then(user => {
-      ctx.body = user
-    })
-    .catch(err => {
-      ctx.body = err
-    })
+  const user = await User.findOne({ username })
+  if (user) {
+    if (user.password === password) ctx.body = user
+    else ctx.body = { error: 'password error' }
+  } else {
+    ctx.body = { error: 'username or password error' }
+  }
 })
 
 module.exports = router
