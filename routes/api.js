ef625766a61f9348c518c3a34e26b10e9a138f6d
ljw1412 <506644791@qq.com>
2019-06-18 18:30:03 +0800

    update api

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,28 @@ router.prefix('/api')
 
 router.get('/list_all_menu', async ctx => {
   ctx.body = [
-    { icon: 'el-icon-location', image: '', title: '导航一', index: '1' },
-    { icon: 'el-icon-location', image: '', title: '导航二', index: '2' },
-    { icon: 'el-icon-setting', image: '', title: '设置', index: '3' }
+    {
+      icon: 'el-icon-location',
+      image: '',
+      title: '导航一',
+      index: '1',
+      route: { name: 'main' },
+      children: []
+    },
+    {
+      icon: 'el-icon-location',
+      image: '',
+      title: '导航二',
+      index: '2',
+      route: { name: 'main' }
+    },
+    {
+      icon: 'el-icon-setting',
+      image: '',
+      title: '设置',
+      index: '3',
+      route: { name: 'setting' }
+    }
   ]
 })
 
