d4d75cebfffd7bc17dd114340ddea8d1fe02878f
ljw1412 <506644791@qq.com>
2019-07-30 14:52:26 +0800

    add pm2 config.

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'development_platform_server',
+      script: 'bin/www',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '300M',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss'
+    }
+  ]
+}
