59a0ac52ae04b15e185155256f4bbd836d1419cd
ljw1412 <506644791@qq.com>
2019-07-30 14:58:56 +0800

    fix.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: true,
-      max_memory_restart: '300M',
       out_file: './logs/out.log',
       error_file: './logs/error.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss'
